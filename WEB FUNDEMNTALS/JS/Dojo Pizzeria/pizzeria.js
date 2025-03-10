12e6ba542ce94b465c7a4428c934e81c787b014c
Bahaa Ennab <bahaenab@gmail.com>
2025-03-10 23:29:53 +0200

    pizzaria

---
+++ WEB FUNDEMNTALS/JS/Dojo Pizzeria/pizzeria.js
@@ -0,0 +1,16 @@
+function pizzaOven(crust, sause, cheeses, toppings) {
+    var pizza = {}
+    pizza.crust = crust
+    pizaa.sause = sause
+    pizza.cheeses = cheeses
+    pizza.toppings = toppings
+    return pizaa
+}
+var p1 = pizzaOven("deep dish", "traditonal", ["mozzarella"], ["pepperoni", "sausage"])
+console.log(p1)
+var p2 = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"])
+console.log(p2)
+var p3 = pizzaOven("hand tossed", "traditional", ["feta"], ["olive", "onions"])
+console.lof(p3)
+var p4 = ("hand tossed", "white sause", ["cheddar"], ["olive", "onions"])
+console.log(p4)
