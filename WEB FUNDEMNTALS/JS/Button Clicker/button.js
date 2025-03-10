684161d43c5e6a6ff90da3a5fe683c44cfe9793d
Bahaa Ennab <bahaenab@gmail.com>
2025-03-10 19:50:35 +0200

    button clicker

---
+++ WEB FUNDEMNTALS/JS/Button Clicker/button.js
@@ -0,0 +1,10 @@
+function txtch(element){
+    if (element.innerText === "Login"){
+        element.innerText = "Logout"
+    }else{
+        element.innerText = "Login"
+    }
+}
+function hide(element){
+    element.remove()
+}
