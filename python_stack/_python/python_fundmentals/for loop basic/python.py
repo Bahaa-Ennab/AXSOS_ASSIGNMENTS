49ecb7f06782e9c0a2b2e0164375e9a847179806
Bahaa Ennab <bahaenab@gmail.com>
2025-03-23 15:54:51 +0200

    loop python

---
+++ python_stack/_python/python_fundmentals/for loop basic/python.py
@@ -0,0 +1,31 @@
+#for count in range (0, 151, 1):
+   # print (count)
+#222222222222222222222222222222222222222222222
+#for multiply in range (5, 1001, 5):
+    #print (multiply)
+#333333333333333333333333333333333333333333333
+# for count in range (1, 101, 1):
+#     if count % 10 == 0:
+#         print("Coding Dojo")
+#     elif count % 5 == 0:
+#         print("Coding ")
+#     else :
+#         print(count)
+#4444444444444444444444444444444444444444444444
+#total = 0
+#for count in range (1, 500001, 2):
+    #total=total+count
+#print(total)
+#5555555555555555555555555555555555555555555555
+#for count in range (2018, 0, -4):
+    #print(count)
+#6666666666666666666666666666666666666666666666
+lowNum = 2
+highNum = 9
+mult = 3
+for count in range (lowNum, highNum+1,1):
+    if count % 3 == 0:
+        print(count)
+
+
+    
