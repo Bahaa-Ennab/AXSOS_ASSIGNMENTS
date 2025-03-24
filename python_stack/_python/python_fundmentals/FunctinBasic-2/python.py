2c314f59143898dae2c6743d62b382256c610d54
Bahaa Ennab <bahaenab@gmail.com>
2025-03-24 18:29:28 +0200

    funcbasic2

---
+++ python_stack/_python/python_fundmentals/FunctinBasic-2/python.py
@@ -0,0 +1,32 @@
+# def countdown(x):
+#     for count in range (x, 0, -1):
+#         print(count)
+# countdown(10)
+# 2222222222222222222222222222222222222222222222
+# def PrintandReturn(a,b):
+#     print(a)
+#     return b
+# print(PrintandReturn(1,2))
+# 333333333333333333333333333333333333333333333333
+# x=[1,2,3,4,5]
+# def plus(x):
+#     return len(x)+x[0]
+# print(plus(x))
+#444444444444444444444444444444444444444444444444444
+# lis=[5,2,3,2,1,4]
+# nlis= []
+# def greater(lis):
+#     if len(lis)<2:
+#         return False
+#     for count in range (0,len(lis),1):
+#         if lis[count] > lis[1]:
+#             nlis.append(lis[count])
+#     print(len(nlis))
+#     return nlis
+# print(greater(lis))
+# 555555555555555555555555555555555555555555555555555
+def length(length,value):
+    return [value] * length
+print(length(6,2))
+    
+
---
+++ python_stack/_python/python_fundmentals/functinBasic_1/FunctionsBasic1.py
@@ -0,0 +1,94 @@
+#1
+def a():
+    return 5  #5
+print(a())
+#2
+# def a(): #10
+#     return 5
+# print(a()+a())
+#3
+# def a(): #5
+#     return 5
+#     return 10
+# print(a())
+# #6
+# def a(b,c): #3,5
+#     print(b+c)
+# print(a(1,2) + a(2,3))
+#7
+# def a(b,c): #2,5
+#     return str(b)+str(c)
+# print(a(2,5))
+#8
+# def a():
+#     b = 100 #100,10
+#     print(b)
+#     if b < 10:
+#         return 5
+#     else:
+#         return 10
+#     return 7
+# print(a())
+# #9
+# def a(b,c): #7,14,21
+#     if b<c:
+#         return 7
+#     else:
+#         return 14
+#     return 3
+# print(a(2,3))
+# print(a(5,3))
+# print(a(2,3) + a(5,3))
+# #10
+# def a(b,c): #8
+#     return b+c
+#     return 10
+# print(a(3,5))
+#11
+# b = 500 #500 ,300,500,300,500
+# print(b)
+# def a():
+#     b = 300
+#     print(b)
+# print(b)
+# a()
+# print(b)
+#12
+# b = 500
+# print(b)
+# def a():
+#     b = 300
+#     print(b)
+#     return b
+# print(b)
+# a()
+# print(b)
+#13
+# b = 500
+# print(b)
+# def a():
+#     b = 300
+#     print(b)
+#     return b
+# print(b)
+# b=a()
+# print(b)
+#14
+# def a():
+#     print(1)
+#     b()
+#     print(2)
+# def b():
+#     print(3)
+# a()
+#15
+def a():
+    print(1)
+    x = b()
+    print(x)
+    return 10
+def b():
+    print(3)
+    return 5
+y = a()
+print(y)
