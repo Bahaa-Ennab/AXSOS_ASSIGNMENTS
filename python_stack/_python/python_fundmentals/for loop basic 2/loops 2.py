c4d7e7f76eeeee3a9c58a7f868408a7235271168
Bahaa Ennab <bahaenab@gmail.com>
2025-03-26 15:18:32 +0200

    loops basic

--- python_stack/_python/python_fundmentals/for loop basic 2/loops 2.py
+++ python_stack/_python/python_fundmentals/for loop basic 2/loops 2.py
@@ -14,7 +14,48 @@
 #     return lis
 # print(posi([-1,1,1,1]))
 # 333333333333333333333333333333333333333333333333333333333333
-def sum(lis):
-    temp = 0
-    for i in range (len(lis)):
-        
+# def sum(lis):
+#     temp = 0
+#     for i in range (len(lis)):
+#         temp = temp +lis[i]
+#     return temp
+# print(sum([1,2,3,4]))
+#4444444444444444444444444444444444444444444444444444444444444
+# def avg(lis):
+#     temp = 0
+#     for i in range (len(lis)):
+#         temp = temp +lis[i]
+#         x=temp/len(lis)
+#     return x
+# print(avg([1,2,3,4]))
+# 5555555555555555555555555555555555555555555555555555555555555
+# def leng(lis):
+#     count = 0
+#     for i in range (0,len(lis),1):
+#         count= count+ 1
+#     return count
+# print(leng([1,2,3,4,5]))
+# 6666666666666666666666666666666666666666666666666666666666666
+# def mini(lis):
+#     for i in range (0,len(lis),1):
+#         return min(lis)
+# print(mini([1,2,3,4,-2]))
+# 77777777777777777777777777777777777777777777777777777777777777
+# def mini(lis):
+#     for i in range (0,len(lis),1):
+#         return max(lis)
+# print(mini([1,2,3,4,-2]))
+# 888888888888888888888888888888888888888888888888888888888888888
+# def all(lis):
+#     for i in range (0,len(lis),1):
+#         a= max(lis)
+#         b=min(lis)
+# print(all([1,2,3,4,-2]))
+# 999999999999999999999999999999999999999999999999999999
+# def rev(lis):
+#         lis.reverse()
+#         return lis
+# print(rev([1,2,3,4,-2]))
+
+
+
