db6102835c0c3b9a852654b9eaa0c7f58593b647
Bahaa Ennab <bahaenab@gmail.com>
2025-04-03 17:32:40 +0300

    checkboard

---
+++ python_stack/_python/python_fundmentals/FuncInter2/FuncInter2.py
@@ -0,0 +1,20 @@
+# x = [ [5,2,3], [10,8,9] ] 
+# students = [
+#      {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#      {'first_name' : 'John', 'last_name' : 'Rosales'}
+# ]
+# sports_directory = {
+#     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+#     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+# }
+# z = [ {'x': 10, 'y': 20} ]
+# x[1][0]=[15]
+# print(x) 
+# students[0]["lastname"]="bryant"
+# print(students[0])
+# sports_directory["soccer"][0]="andres"
+# print(sports_directory["soccer"])
+# z[0]["y"]="30"
+# print(z)
+#3333333333333333333333333333333333333333333333333333333333333333333333333
+..............e'
