dc67f3593de7c89d6035ff39095a140ecd8b0b9d
BAhaa Enab <bahaenab@gmail.com>
2025-06-30 15:02:08 +0300

    first assignments

---
+++ MERN_Stack/hoist.js
@@ -0,0 +1,83 @@
+var hello;
+console.log(hello);
+hello="world";
+
+// -------------------------------------------1-----------------------------------------------
+
+var needle = 'haystack'
+
+test()
+function test(){
+    var needle
+    needle = 'magnet'
+    console.log(needle);
+}
+
+// -------------------------------------------2-----------------------------------------------
+
+var brendan
+brendan ='super cool'
+function print(){
+    brendan = 'Only Okay'
+    console.log(brendan);
+}
+console.log(brendan);
+
+// -------------------------------------------3-----------------------------------------------
+
+var food 
+food='chicken'
+console.log(food);
+
+eat()
+function eat(){
+    var food
+    food='half-chicken';
+    console.log(food);
+    food = 'gone'
+}
+
+
+// -------------------------------------------4-----------------------------------------------
+
+var mean
+mean()
+console.log(food);
+mean = function(){
+    var food
+    food='chicken'
+    console.log(food);
+    food='fish'
+    console.log(food);
+}
+console.log(food);
+// -------------------------------------------5-----------------------------------------------
+
+var genre 
+console.log(genre);
+genre = 'disco';
+rewind();
+function rewind(){
+    var genre
+    genre = 'rock';
+    console.log(genre);
+    genre = 'r&b'
+    console.log(genre);
+}
+console.log(genre)
+// -------------------------------------------6-----------------------------------------------
+
+var dojo;
+dojo = "san jose";
+console.log(dojo);
+learn();
+function learn() {
+    var dojo;
+    dojo = "seattle"; 
+    console.log(dojo);
+
+     dojo = "burbank";
+      console.log(dojo);
+}
+console.log(dojo);
+// -------------------------------------------7-----------------------------------------------
