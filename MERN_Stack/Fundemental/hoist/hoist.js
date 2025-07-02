73d4424fccbd8bd9afca89c24c12389c31372d48
BAhaa Enab <bahaenab@gmail.com>
2025-07-02 14:49:54 +0300

    edit

--- MERN_Stack/Fundemental/hoist/hoist.js
+++ MERN_Stack/Fundemental/hoist/hoist.js
@@ -1,83 +1,84 @@
-var hello;
-console.log(hello);
-hello="world";
+// var hello;
+// console.log(hello);//undefined
+// hello="world";
 
-// -------------------------------------------1-----------------------------------------------
+// // -------------------------------------------1-----------------------------------------------
+ 
+// var needle = 'haystack'
+ 
+// test()
+// function test(){
+//     var needle
+//     needle = 'magnet'
+//     console.log(needle);//magnet
+// }
+ 
+// // -------------------------------------------2-----------------------------------------------
+ 
+// var brendan
+// brendan ='super cool'
+// function print(){
+//     brendan = 'Only Okay'
+//     console.log(brendan);//the function is not called
+// }
+// console.log(brendan);//super cool
 
-var needle = 'haystack'
-
-test()
-function test(){
-    var needle
-    needle = 'magnet'
-    console.log(needle);
-}
-
-// -------------------------------------------2-----------------------------------------------
-
-var brendan
-brendan ='super cool'
-function print(){
-    brendan = 'Only Okay'
-    console.log(brendan);
-}
-console.log(brendan);
-
-// -------------------------------------------3-----------------------------------------------
-
-var food 
-food='chicken'
-console.log(food);
-
-eat()
-function eat(){
-    var food
-    food='half-chicken';
-    console.log(food);
-    food = 'gone'
-}
-
-
-// -------------------------------------------4-----------------------------------------------
-
-var mean
-mean()
-console.log(food);
-mean = function(){
-    var food
-    food='chicken'
-    console.log(food);
-    food='fish'
-    console.log(food);
-}
-console.log(food);
-// -------------------------------------------5-----------------------------------------------
-
-var genre 
-console.log(genre);
-genre = 'disco';
-rewind();
-function rewind(){
-    var genre
-    genre = 'rock';
-    console.log(genre);
-    genre = 'r&b'
-    console.log(genre);
-}
-console.log(genre)
-// -------------------------------------------6-----------------------------------------------
-
+ 
+// // -------------------------------------------3-----------------------------------------------
+ 
+// var food
+// food='chicken'
+// console.log(food);//chicken
+ 
+// eat()
+// function eat(){
+//     var food
+//     food='half-chicken';
+//     console.log(food);//half-chicken
+//     food = 'gone'
+// }
+ 
+ 
+// // -------------------------------------------4-----------------------------------------------
+ 
+// var mean
+// mean()//this caused an error because mean is not function
+// console.log(food);
+// mean = function(){
+//     var food
+//     food='chicken'
+//     console.log(food);
+//     food='fish'
+//     console.log(food);
+// }
+// console.log(food);
+// // -------------------------------------------5-----------------------------------------------
+ 
+// var genre
+// console.log(genre);//undefined
+// genre = 'disco';
+// rewind();
+// function rewind(){
+//     var genre
+//     genre = 'rock';
+//     console.log(genre);//rock
+//     genre = 'r&b'
+//     console.log(genre);//r&b
+// }
+// console.log(genre)//disco
+// // -------------------------------------------6-----------------------------------------------
+ 
 var dojo;
 dojo = "san jose";
-console.log(dojo);
+console.log(dojo);//san jose
 learn();
 function learn() {
     var dojo;
-    dojo = "seattle"; 
-    console.log(dojo);
-
+    dojo = "seattle";
+    console.log(dojo);//seattle
+ 
      dojo = "burbank";
-      console.log(dojo);
+      console.log(dojo);//burbank
 }
-console.log(dojo);
-// -------------------------------------------7-----------------------------------------------
+console.log(dojo);//san jose
+// // -------------------------------------------7-----------------------------------------------
