c040797293a64529a83cc40caa3823106b8e418c
BAhaa Enab <bahaenab@gmail.com>
2025-07-02 13:57:08 +0300

    first jsx

---
+++ MERN_Stack/React/first_jsx/fisrt_jsx/client/vite-project/src/components/AppComponent.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const AppComponent = () =>{
+    return(
+        <>
+    <h1>Hello Dojo</h1>
+    <h2>Things I need do:</h2>
+    <ul>
+        <li>learn react</li>
+        <li>learn react</li>
+        <li>learn react</li>
+        <li>learn react</li>
+    </ul>
+    </>
+)
+}
+export default AppComponent;
